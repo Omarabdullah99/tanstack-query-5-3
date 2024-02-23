8df98f6e1d2c5ef7248f846affa847173f603747
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-02-23 21:45:07 +0600

    GetProduct-tansk-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import ProductList from './component/ProductList'
 export default function App() {
   return (
-    <div className="text-orange-500 text-center text-6xl font-bold">
-      TanStack query course
+    <div >
+      <ProductList />
     </div>
   );
 }
